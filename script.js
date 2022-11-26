2d5b641bb9a41dfa7da32d8234062f6c09319197
Randol <81429286+randol205@users.noreply.github.com>
2022-11-26 12:35:09 -0600

    a

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 var isDarkMode=false
 
 function toggleTheme() {
-    if (isDarkMode) {
-        isDarkMode=!isDarkMode
-    }
-}
+    isDarkMode=!isDarkMode    
+
 if(isDarkMode) {
-    document.body.classList.add("dark")
-    //Do something if DarkMode is true 
+    document.body.classList.add("dark")    
 } else {
     document.body.classList.remove('dark')
-    //Do something if DarkMode is false 
+  }  
 }
